f94c565647424f8ebec00fe3462e35591a2a5c55
Aidan Heller <asfheller@gmail.com>
2018-10-23 19:14:30 -0700

    Finish initial subsystem

--- src/main/java/org/sert2521/bunnybots/intake/Intake.kt
+++ src/main/java/org/sert2521/bunnybots/intake/Intake.kt
@@ -1,16 +1,14 @@
 package org.sert2521.bunnybots.intake
 
 import org.sert2521.bunnybots.intake.commands.IntakeControl
+import org.sert2521.bunnybots.util.INTAKE_MOTOR
 import org.sertain.command.Subsystem
-import org.sertain.hardware.DigitalInput
 import org.sertain.hardware.Talon
 
 object Intake : Subsystem() {
     const val INTAKE_SPEED = 0.0
 
-    val motor = Talon(0)
-    val switch = DigitalInput(0)
+    val motor = Talon(INTAKE_MOTOR)
 
-    override val defaultCommand
-        get() = IntakeControl()
+    override val defaultCommand get() = IntakeControl()
 }
--- src/main/java/org/sert2521/bunnybots/intake/commands/IntakeControl.kt
+++ src/main/java/org/sert2521/bunnybots/intake/commands/IntakeControl.kt
@@ -1,18 +1,20 @@
 package org.sert2521.bunnybots.intake.commands
 
 import org.sert2521.bunnybots.intake.Intake
+import org.sert2521.bunnybots.util.secondaryJoystick
 import org.sertain.command.Command
 
-class IntakeControl() : Command() {
+class IntakeControl : Command() {
     init {
         requires(Intake)
     }
 
-    var shouldRun = false
+    private val shouldToggle get() = secondaryJoystick.getRawButton(-1)
+    private var shouldRun = false
 
     override fun execute(): Boolean {
         Intake.motor.set(if (shouldRun) Intake.INTAKE_SPEED else 0.0)
-        if (Intake.switch.get()) shouldRun = !shouldRun
+        if (shouldToggle) shouldRun = !shouldRun
         return false
     }
 }
