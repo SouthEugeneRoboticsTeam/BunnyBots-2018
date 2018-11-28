a4147ab8164a2f001d4c153a143ddf0967c62f1d
Carkoth <danielbrase.carkoth@gmail.com>
2018-11-27 18:06:36 -0800

    Begin switching from toggle to hold

--- src/main/java/org/sert2521/bunnybots/outtake/commands/OuttakeControl.kt
+++ src/main/java/org/sert2521/bunnybots/outtake/commands/OuttakeControl.kt
@@ -4,19 +4,24 @@ import org.sert2521.bunnybots.outtake.Outtake
 import org.sert2521.bunnybots.util.OUTTAKE_BELT_MOTOR
 import org.sert2521.bunnybots.util.secondaryJoystick
 import org.sertain.command.Command
+import edu.wpi.first.wpilibj.Servo
 
 class OuttakeControl : Command() {
     init {
         requires(Outtake)
     }
 
-    //private val shouldToggle get() = secondaryJoystick.getRawButton(-1)
-a
-    private var shouldRun = false
+    private val shouldOutput get() = secondaryJoystick.getRawButton(-1)
 
     override fun execute(): Boolean {
         /*Outtake.motor.set(if (shouldRun) Outtake.OUTTAKE_SPEED else 0.0)
         if (shouldToggle) shouldRun = !shouldRun*/
+        if (shouldOutput) {
+
+            Outtake.beltMotor.set(Outtake.OUTTAKE_BELT_SPEED)
+            Outtake.flapServo.setAngle(0.0)
+            
+        }
 
         return false
 
