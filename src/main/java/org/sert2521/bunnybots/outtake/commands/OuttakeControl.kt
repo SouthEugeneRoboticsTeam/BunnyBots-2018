0deee51aced23e6f8ac267f1cf4275f99bad2859
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-01 18:37:41 -0800

    Update OuttakeControl.kt

--- src/main/java/org/sert2521/bunnybots/outtake/commands/OuttakeControl.kt
+++ src/main/java/org/sert2521/bunnybots/outtake/commands/OuttakeControl.kt
@@ -14,20 +14,16 @@ class OuttakeControl : Command() {
     private val shouldOutput get() = secondaryJoystick.getRawButton(-1)
 
     override fun execute(): Boolean {
-
-        // If joystick button is pressed, open flap and start belt motor. If the button is not being pressed, close the flap and set the motor speed to 0 
+        // If joystick button is pressed, open flap and start belt motor.
+        // If the button is not being pressed, close the flap and set the motor speed to 0 
         if (shouldOutput) {
-
             Outtake.beltMotor.set(Outtake.OUTTAKE_BELT_SPEED)
             Outtake.flapServo.angle = 90.0
-
         } else {
-
             Outtake.beltMotor.set(0.0)
             Outtake.flapServo.angle = 0.0
-
         }
+        
         return false
-
     }
 }
