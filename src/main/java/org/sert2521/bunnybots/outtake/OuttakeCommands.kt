44b3a3d26e21ff3f96613c20b767a35db2333b31
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-15 21:42:55 -0800

    Add more outtake methods

--- src/main/java/org/sert2521/bunnybots/outtake/OuttakeCommands.kt
+++ src/main/java/org/sert2521/bunnybots/outtake/OuttakeCommands.kt
@@ -1,10 +1,20 @@
 package org.sert2521.bunnybots.outtake
 
+import edu.wpi.first.wpilibj.Timer
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
 suspend fun runOuttake() = use(Outtake) {
     try {
+        val timer = Timer().apply { start() }
+        periodic {
+            Outtake.run(-OUTTAKE_BELT_SPEED)
+
+            if (timer.get() >= 0.25) exitPeriodic()
+        }
+
+        timer.stop()
+
         periodic {
             Outtake.run()
         }
@@ -14,5 +24,9 @@ suspend fun runOuttake() = use(Outtake) {
 }
 
 suspend fun openFlap() = use(Outtake) {
-    Outtake.open(true)
+    try {
+        Outtake.toggle()
+    } finally {
+        println("Done toggling")
+    }
 }
