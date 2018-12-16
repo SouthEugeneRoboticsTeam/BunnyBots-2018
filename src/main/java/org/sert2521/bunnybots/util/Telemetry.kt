a323f73d687030bb2c655488b4f4f266429809d8
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-15 23:07:14 -0800

    Don't watch for overrun in telemetry

--- src/main/java/org/sert2521/bunnybots/util/Telemetry.kt
+++ src/main/java/org/sert2521/bunnybots/util/Telemetry.kt
@@ -39,7 +39,7 @@ class Telemetry {
 
     init {
         TelemetryScope.launch {
-            periodic(period = 0.1) {
+            periodic(period = 0.1, watchOverrun = false) {
                 tick()
             }
         }
