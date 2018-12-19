8ae5900accd1de26ddc2ab3ce5299316d5b394ca
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-19 10:24:26 -0800

    Remove unused import

--- src/main/java/org/sert2521/bunnybots/outtake/OuttakeCommands.kt
+++ src/main/java/org/sert2521/bunnybots/outtake/OuttakeCommands.kt
@@ -1,7 +1,6 @@
 package org.sert2521.bunnybots.outtake
 
 import org.sert2521.bunnybots.util.timedPeriodic
-import org.team2471.frc.lib.coroutines.delay
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.use
 
